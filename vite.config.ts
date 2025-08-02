7d2396a557addd6a81c7d563ee693be73e5175c2
Mosch12 <136726718+12Mosch@users.noreply.github.com>
2025-08-02 14:46:57 +0200

    feat: Add react-compiler (#8)

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,14 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwind()],
+	plugins: [
+		react({
+			babel: {
+				plugins: ["babel-plugin-react-compiler"],
+			},
+		}),
+		tailwind(),
+	],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
